876e768c42f65fdbfe7d790573f412636e3c000c
dverg <kristian@dverg.net>
2016-01-20 11:39:57 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const Hapi = require('hapi');
+const Good = require('good');
+
+const server = new Hapi.Server();
+server.connection({
+		host: 'localhost',
+		port: 8000
+});
+
+// Routes (more to come)
+server.route({
+		method: 'GET',
+		path: '/',
+		handler: function(request, reply){
+				return reply('Hello æ <');
+		}
+});
+
+// Static files served from here
+server.register(require('inert'), (err) => {
+		if (err){
+				throw err;
+		}
+
+		server.route({
+				method: 'GET',
+				path: '/html',
+				handler: function(request, reply){
+					return reply.file('views/game_table.html');
+				}
+		});
+});
+
+server.register({
+		register: Good,
+		options: {
+				reporters: [{
+						reporter: require('good-console'),
+						events: {
+								response: '*',
+								log: '*'
+						}
+				}]
+		}
+}, (err) => {
+		if (err) {
+				throw err;
+		}
+
+		
+
+		server.start(() => {
+				console.log('info', 'Server running at :', server.info.uri);
+		});
+});
+
